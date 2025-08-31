f77e2dff3de979387a5fe98716dbb09d26d80bdd
rishi233171 <rishi233171@gmail.com>
2025-08-31 10:18:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import DashboardPortfolio from "./components/DashboardPortfolio";
 import { Toaster } from "./components/ui/toaster";
@@ -8,13 +7,13 @@ import { Toaster } from "./components/ui/toaster";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
 
-const Home = () => {
+function App() {
   const helloWorldApi = async () => {
     try {
       const response = await axios.get(`${API}/`);
       console.log(response.data.message);
     } catch (e) {
-      console.error(e, `errored out requesting / api`);
+      console.log("API not available, using mock data");
     }
   };
 
@@ -22,19 +21,9 @@ const Home = () => {
     helloWorldApi();
   }, []);
 
-  return <DashboardPortfolio />;
-};
-
-function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            <Route index element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <DashboardPortfolio />
       <Toaster />
     </div>
   );
