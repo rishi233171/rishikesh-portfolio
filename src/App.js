8baf8f53a11245d624db3cda90105c137ce6db6d
rishi233171 <rishi233171@gmail.com>
2025-08-31 10:22:42 +0530

    Update App.js
    Remove axios dependency

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import { useEffect } from "react";
 import "./App.css";
-import axios from "axios";
 import DashboardPortfolio from "./components/DashboardPortfolio";
 import { Toaster } from "./components/ui/toaster";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API = `${BACKEND_URL}/api`;
-
 function App() {
-  const helloWorldApi = async () => {
-    try {
-      const response = await axios.get(`${API}/`);
-      console.log(response.data.message);
-    } catch (e) {
-      console.log("API not available, using mock data");
-    }
-  };
-
-  useEffect(() => {
-    helloWorldApi();
-  }, []);
-
   return (
     <div className="App">
       <DashboardPortfolio />
